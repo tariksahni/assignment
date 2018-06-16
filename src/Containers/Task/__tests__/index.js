2a7b39401d4233c86eab7182aa3c85efe529f112
Tarik Sahni <tarik_sahni@zykrr.com>
2018-06-16 19:24:13 +0530

    Test Added

---
+++ src/Containers/Task/__tests__/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { Provider } from 'react-redux'
+import { shallow } from 'enzyme'
+import { withRouter } from 'react-router'
+import renderer from 'react-test-renderer'
+import configureStore from '../../../Store'
+import { Task } from '../../'
+
+const store = configureStore()
+
+configure({ adapter: new Adapter() })
+
+describe(Task, () => {
+  const Component = shallow(<Provider store={store}><Task /></Provider>)
+  const WrappedComponent = withRouter(Component)
+  it('renders and matches our snapshot', () => {
+    const Component = <Provider store={store}><Task /></Provider>
+    const WrappedComponent = withRouter(Component)
+    const component = renderer.create(WrappedComponent)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('contains the supplied name', () => {
+    expect(WrappedComponent.text()).toContain('<Connect(withRouter(Task)) />')
+  })
+})
